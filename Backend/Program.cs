dd2b14e7b0cfc08985efecc7e4d66aae4f22833b
klaudia <ksawicka2004.ks@gmail.com>
2026-06-10 01:37:34 +0200

    fixes

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -49,7 +49,7 @@ namespace ProjektTnai
             builder.Services.AddCors(options =>
             {
                 options.AddPolicy("AllowFrontend", policy =>
-                    policy.WithOrigins("http://localhost:3000", "http://localhost:5173")
+                    policy.AllowAnyOrigin()
                           .AllowAnyHeader()
                           .AllowAnyMethod());
             });
