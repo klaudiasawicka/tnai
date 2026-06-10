f6ba3cde879840942e254d1105d2cf922f7d74a5
klaudia <ksawicka2004.ks@gmail.com>
2026-06-10 17:24:51 +0200

    azure fixes

---
+++ Backend/Migrations/20260610150240_AddedTablesToAzureDb.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjektTnai.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedTablesToAzureDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
